8e687c7db50af4967b105adc09225dfe27be2de8
troinich <troinicz@gmail.com>
2019-03-15 10:39:37 -1000

    NewMessage Component created

--- online-chat/src/components/Chat.vue
+++ online-chat/src/components/Chat.vue
@@ -1,22 +1,48 @@
 <template>
-    <div class="chat container">
-        <h2>Chat {{this.name}} </h2>
+  <div class="chat container">
+    <h2 class="center teal-text">Online Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">Message</span>
+            <span class="grey-text time">Time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+          <NewMessage :name="name"/>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
+
 export default {
-    name: 'Chat',
-    //prop that we accept from welcome
-    props:['name'],
-
-    data(){
-        return{
-
-        }
-    }
-}
+  name: "Chat",
+  //prop that we accept from welcome
+  props: ["name"],
+  components: {
+      NewMessage},
+  data() {
+    return {};
+  }
+};
 </script>
 <style>
+.chat h2{
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span{
+    font-size: 1.4em;
+}
+.chat .time{
+    display: block;
+    font-size: 1.2em;
+}
 
 </style>
--- online-chat/src/router/index.js
+++ online-chat/src/router/index.js
@@ -14,7 +14,14 @@ export default new Router({
       path: '/chat',
       name:'Chat',
       component: () => import('@/components/Chat.vue'),
-      props: true
+      props: true,
+      beforeEnter: (to, from, next)=> {
+        if (to.params.name){
+          next()
+        } else{
+          next({name: "Welcome"})
+        }
+      }
     }
   ]
 })
